480214c240a001cf5935f8c4402ea7109fca7c51
Muhumed <Muhumed@pdx.edu>
2023-04-07 19:43:24 -0700

    Assignment 1

---
+++ Video Assignments/Assignemnt 1/1/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//  gcc -Wall -o hello hello.c   --> compiles code 
+//  ./hello --> runs code 
+
+int main(void){
+
+    printf("Hello cs201!!\n");
+    return 0;
+}
---
+++ Video Assignments/Assignemnt 1/2/f2c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// gcc -Wall -g -o f2c f2c.c
+// ./f2c 
+
+/*
+Fahrenheit Celsius 
+32          0.0000
+40          4.4444
+50          10.000
+60          15.1556
+100         37.7778
+212         100.000
+*/
+
+int main(void){
+
+    int fahrenheit = 0;
+    int celsius = 0;
+    
+}
