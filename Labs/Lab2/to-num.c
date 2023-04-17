c88f6275b7e5d8df2015bab95077ff07e68e3fb9
Zakeriya Muhumed <87618816+Zakeriya23@users.noreply.github.com>
2023-04-16 23:37:00 -0700

    Update to-num.c

--- Labs/Lab2/to-num.c
+++ Labs/Lab2/to-num.c
@@ -12,37 +12,27 @@ int main(void){
     char str1[STRLEN]= {'\0'};
     int i =0;
     fgets(str1,STRLEN,stdin);
-    
-    {//Char 
-        printf("character output\n");
-        while(str1[i] != '\0'){
-            printf("%c ", str1[i++]);
-        }
 
+    printf("character output\n");
+    while(str1[i] != '\0'){
+       printf("%c ", str1[i++]);
     }
-
-    {//Octal
-        printf("octal output\n");
-        i=0;
-        while(str1[i] != '\0'){
-                printf("%#o ", str1[i++]);
-        }
-    }
-    {//decimal
-        printf("\ndecimal output\n");
-        i=0;
-        while(str1[i] != '\0'){
-                printf("%d ", str1[i++]);
-        }
-    }
-    {//decimal
-        printf("\nhex output\n");
-        i=0;
-        while(str1[i] != '\0'){
-                printf("%#X ", str1[i++]);
-        }
-        printf("\n");
-    }
+    printf("octal output\n");
+     i=0;
+     while(str1[i] != '\0'){
+     printf("%#o ", str1[i++]);
+     }
+     printf("\ndecimal output\n");
+     i=0;
+     while(str1[i] != '\0'){
+        printf("%d ", str1[i++]);
+     }
+     printf("\nhex output\n");
+     i=0;
+     while(str1[i] != '\0'){
+        printf("%#X ", str1[i++]);
+     }
+     printf("\n");
 
     return 0;
 }
